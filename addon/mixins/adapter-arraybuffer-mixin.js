fc14be6ec6ccb3fd4f4bb1bd6a8ad273e4c03335
Anthony Pessy <anthony.pessy@gmail.com>
2016-12-01 16:57:29 +0100

    add a mixin for ember data adapter that will set the proper dataType for arraybuffer/blob requests

---
+++ addon/mixins/adapter-arraybuffer-mixin.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+
+  /**
+   * Because dataType:json is hardcoded we are looking
+   * for either a blob:true or arraybuffer:true ajax option
+   * in order to set the proper data type to jQuery.
+   *
+   * @param url
+   * @param type
+   * @param options
+   * @returns {*}
+   */
+  ajaxOptions(url, type, options) {
+    const opts = this._super(url, type, options);
+    if (options.arraybuffer) {
+      opts.dataType = 'arraybuffer';
+      opts.processData = false;
+    } else if (options.blob) {
+      opts.dataType = 'blob';
+      opts.processData = false;
+    }
+    return opts;
+  }
+
+});
